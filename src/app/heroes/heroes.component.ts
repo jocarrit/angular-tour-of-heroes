f851f66ed39fb6cebcfcf4f70fe3ff116c09e5ad
Jose M Carrillo <jmcarrillo@ibm.com>
2021-07-01 15:20:50 -0600

    get hero data from service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
 import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,21 +9,26 @@ import { HEROES } from '../mock-heroes'
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // }
 
-  heroes = HEROES
+  heroes: Hero[] = []
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes()
+  }
 
   selectedHero?: Hero
   onSelect(hero: Hero): void {
     this.selectedHero = hero
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes()
   }
 
 }
